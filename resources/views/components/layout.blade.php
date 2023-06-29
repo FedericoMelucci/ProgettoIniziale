6d7cb34b1a1fccc40e69d7b6c708848b77a6005a
federico melucci <fede.melufm97@outlook.it>
2023-06-29 16:12:19 +0200

    Questo è il progetto di base

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    @vite(['resource/css/app.css','reources/js/app.js'])
+  </head>
+  <body>
+    <x-navbar />
+    <h1>Titolo</h1>
+
+    <div>
+        {{ $slot }}
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <x-footer />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
